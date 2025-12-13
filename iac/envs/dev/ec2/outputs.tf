2e759818a4eb92916752cad50f4688387ab057ee
Sabir MOHAMED <sabir.mba@protonmail.com>
2025-12-13 22:01:31 +0100

    chore: clean up Terraform configuration by removing unused output variables and adding newlines for better readability

--- iac/envs/dev/ec2/outputs.tf
+++ iac/envs/dev/ec2/outputs.tf
@@ -2,21 +2,6 @@ output "mongo_private_ip" {
   value = aws_instance.mongo.private_ip
 }
 
-output "mongo_public_ip" {
-  description = "Public IP address of the Mongo instance"
-  value       = aws_instance.mongo.public_ip
-}
-
-output "mongo_ssh_user" {
-  description = "SSH username for the Mongo instance"
-  value       = "ubuntu"
-}
-
 output "mongo_sg_id" {
   value = aws_security_group.mongo.id
-}
-
-output "backup_bucket_name" {
-  description = "Name of the S3 bucket for Mongo backups"
-  value       = aws_s3_bucket.backups.id
 }
