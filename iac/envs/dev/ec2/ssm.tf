c4e5bab9c6ce1cc1088bc82e0227d015d641874c
Sabir MOHAMED <sabir.mba@protonmail.com>
2026-03-01 16:06:32 +0100

    feat: Implement dynamic fetching of MongoDB credentials from SSM Parameter Store in backup script and add SSM parameters for MongoDB admin user and password in Terraform configuration.

--- iac/envs/dev/ec2/ssm.tf
+++ iac/envs/dev/ec2/ssm.tf
@@ -7,6 +7,24 @@ resource "aws_ssm_parameter" "mongodb_uri" {
   tags = local.tags
 }
 
+resource "aws_ssm_parameter" "mongo_admin_user" {
+  name        = "/wiz-exercise/dev/mongo-admin-user"
+  description = "MongoDB admin username"
+  type        = "SecureString"
+  value       = var.mongo_admin_user
+
+  tags = local.tags
+}
+
+resource "aws_ssm_parameter" "mongo_admin_password" {
+  name        = "/wiz-exercise/dev/mongo-admin-password"
+  description = "MongoDB admin password"
+  type        = "SecureString"
+  value       = var.mongo_admin_password
+
+  tags = local.tags
+}
+
 resource "aws_ssm_parameter" "secret_key" {
   name        = "/wiz-exercise/dev/secret-key"
   description = "JWT secret key for the application"
